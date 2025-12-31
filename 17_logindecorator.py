a0e7ee4bd9d7d8d064c9f563943687355f483a81
Utkarshjha09 <utkarshjha.999@gmail.com>
2025-12-31 18:38:14 +0530

    updated for understanding

--- 17_logindecorator.py
+++ 17_logindecorator.py
@@ -1,6 +1,6 @@
 from functools import wraps
 def log_activity(func):
-    @wraps(func)
+    @wraps(func) #this is used because if we want to check the name of the function it will tell me the name of that function which is passed in to main function 
     def wrapper(*args, **kwargs):
         print(f"Calling: {func.__name__}")
         result = func(*args,**kwargs)
@@ -8,7 +8,7 @@ def log_activity(func):
         return result
     return wrapper
 
-@log_activity
+@log_activity # calling the above function which is defined # humm "Book_Ticket" function ko "log_activity" function mai daal rhe hai 
 def Book_Ticket(type):
-    print(f"Booking {type}  Ticket")
-Book_Ticket("Stranger.Things")
+    print(f"Booking {type} Ticket")
+Book_Ticket("Stranger.Things") # passing "Stranger.Things" argument in this "Book_Ticket" function 
