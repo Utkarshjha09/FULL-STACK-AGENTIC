ed654544ab2d8715e5d75e7ed2c7f2fecdc37707
Utkarshjha09 <utkarshjha.999@gmail.com>
2026-01-01 23:08:28 +0530

    Starting OOPS in PY

---
+++ OOPS/01_simpleclass.py
@@ -0,0 +1,9 @@
+class Movie:
+    pass
+class MovieTime:
+    pass
+print(type(Movie))
+ComedyMovie_3idiot = Movie()
+print(type(ComedyMovie_3idiot))
+print(type(ComedyMovie_3idiot) is Movie)
+print(type(ComedyMovie_3idiot) is MovieTime)
