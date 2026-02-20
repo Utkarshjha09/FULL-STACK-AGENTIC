51350b54e7cd2a4b18682c4cfd62c81a4f142907
Utkarshjha09 <utkarshjha.999@gmail.com>
2026-02-20 11:37:30 +0530

    Custom exception emaple 2 added

---
+++ Exceptionalhandling/06_Custom_exceptions_02.py
@@ -0,0 +1,9 @@
+class OutofIngredientsError(Exception): # yaha humne ek class banaya "OutofIngredientsError" name ka phir  usmai ek function banaya 
+    pass
+
+def make_chai(milk,sugar): # humne ek function banaya by taking input mai milk or sugar diya 
+    if milk == 0 or sugar==0: # agar chai ya phir sugar dono mai se ek v "0" hua toh "OutofIngredientsError" raise karegaa
+        raise OutofIngredientsError("Missing milk or sugar") 
+    print ("Chai is Ready")
+
+make_chai(0,1) # yaha humne function call kiya hai by giving input sugar as 1 and milk as 0
