a4dad5e0626e166dbdee7c10a849115782e7e368
Utkarshjha09 <utkarshjha.999@gmail.com>
2026-05-25 05:16:00 +0530

    Renamed multithreading files and updated examples

--- 02_Multithreading/04_globalInterpreterMultiproccessing.py
+++ 02_Multithreading/04_globalInterpreterMultiproccessing.py
@@ -4,21 +4,21 @@ import time
 def crunch_number():
     print(f"Started the count process...")
     count = 0
-    for _ in range(100_000_000):
+    for _ in range(100_000_000):  # yaha humne fuction create kiya hai initially count '0' tha at every vary count get increases with the '1' 
         count += 1
-    print(f"Ended the count process...")
+    print(f"Ended the count process...") 
 
 if __name__ == "__main__":
-    start = time.time()
+    start = time.time() # yaha humne thread start kiya hai 
 
-    p1 = Process(target=crunch_number)
-    p2= Process(target=crunch_number)
+    p1 = Process(target=crunch_number) # yeh ek thread banaya hai
+    p2= Process(target=crunch_number)   # yeh dusra thread hai 
 
-    p1.start()
+    p1.start() # yaha thread initialize karne ka baad humne start kiya hai 
     p2.start()
-    p1.join()
-    p2.join()
+    p1.join() # yaha thread ko join kiya hai kyuki in case if alag alag threads ka alag alag sleep time ho skta hai toh alag alag execute hoga 
+    p2.join()   # so we get value a different time so we join this threads 
 
-    end = time.time()
+    end = time.time() # yaha thread end hoga yeh hai 
 
-    print(f"Total time with multi-processing is {end - start:.2f} seconds")
+    print(f"Total time with multi-processing is {end - start:.2f} seconds")  # yaha humne print kara diya hume start se end mai kitna time laga hai.
--- 02_Multithreading/07_downloadThread.py
+++ 02_Multithreading/07_downloadThread.py
@@ -1,28 +1,36 @@
+#######  SHADOW POINT     ########
+# Image download karne ke liye v threads ko use kar skte hai 
+# "requests.get" ka use karte hai hum kuch v download karne ke liye urls 
+# ".content" ka use karte hai hum size janane ke liye But " Woh size BYTES mai deta hai"
+# remeber thread ke target mai hum function ka target dete hai and args mai input pass karte hai 
+
+
+
 import threading
 import requests
 import time
 
-def download_thread(url):
+def download_thread(url):  # ek function banaya input url jisko humne ek array liya hai 
     print(f"Starting Download from{url}")
-    resp = requests.get(url)
-    print (f" Finished Downloading from the {url}, size: {len(resp.content)} bytes")
+    resp = requests.get(url) # pick kar rhe hai url mai se and download karne ka request kar rhe 
+    print (f" Finished Downloading from the {url}, size: {len(resp.content)} bytes") 
 
-urls =[
+urls =[  # yeh array hai links ki
 "https://httpbin.org/image/jpeg"
 "https://httpbin.org/imagr/png"
 "https://httpbin.org/imagr/svg"
 ]
-start =time.time()
+start =time.time()  # yaha se start time track kar rhe hai 
 threads = []
 
-for url in urls:
-    t = threading.Thread(target= download_thread, args =(url,))
-    t.start()
-    threads.append(t)
+for url in urls: # yaha ek 'for' loop chalaya for each element in the array 
+    t = threading.Thread(target= download_thread, args =(url,)) # argument pass kiya taget mai jo ki function call kar rha url
+    t.start() # yaha thread (process) start hua hai 
+    threads.append(t)  #aur hum isko append kar rhe hai "t" 
 
 for t in threads:
-    t.join()
-
+    t.join() # maan loa koi image kam mb ka hai toh koi jada ka to koi pehle download ho jayega alag alag url se  koi baad mai toh koi pehle alag alag
+            # toh humne yeh join use kiya hai taaki sab ek saath dikhe humko chai kyuhie alag ho but dikhe ek sath terminal mai.
 end = time.time()
-
-print (f"The total time taken is {end-start :.2f} second")
+# yaha pe time track baand kar denge 
+print (f"The total time taken is {end-start :.2f} second")  
